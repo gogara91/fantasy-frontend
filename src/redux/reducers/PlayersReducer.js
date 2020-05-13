6042509524d9f45e42c430d899599839df562bb2
Goran Nikolajevic <gorans91@gmail.com>
2020-05-13 18:14:49 +0200

    commit

--- src/redux/reducers/PlayersReducer.js
+++ src/redux/reducers/PlayersReducer.js
@@ -1,8 +1,12 @@
-import * as actionTypes from '../actions/actionTypes'
+import * as actionTypes from '../actions/actionTypes';
+import PlayersService from "../../services/PlayersService";
 
 const initialState = {
     playersWithTeam: [],
-    FilteredPayersWithTeam: []
+    filteredPayersWithTeam: [],
+    playerNameFilter: '',
+    sortByFilter: '',
+    teamsFilter: []
 };
 
 export const PlayersStore = (state = initialState, action) => {
@@ -11,9 +15,42 @@ export const PlayersStore = (state = initialState, action) => {
             return {
                 ...state,
                 playersWithTeam: action.payload,
-                FilteredPayersWithTeam: action.payload
+                filteredPayersWithTeam: action.payload
             };
-        default:
-            return state;
+        case actionTypes.FILTER_PLAYERS_BY_NAME:
+            return {
+                ...state,
+                playerNameFilter: action.payload,
+                filteredPayersWithTeam: PlayersService.filterPlayers(
+                    state.playersWithTeam,
+                    action.payload,
+                    state.teamsFilter,
+                    state.sortByFilter
+                )
+            };
+        case actionTypes.SET_SORT_BY_FILTER:
+            return {
+                ...state,
+                sortByFilter: action.payload
+            };
+        case actionTypes.SET_TEAMS_FILTER:
+            return {
+                ...state,
+                teamsFilter: action.payload,
+                filteredPayersWithTeam: PlayersService.filterPlayers(
+                    state.playersWithTeam,
+                    state.playerNameFilter,
+                    action.payload,
+                    state.sortByFilter
+                )
+            };
+        case actionTypes.RESET_PLAYER_FILTERS:
+            return {
+                ...state,
+                playerNameFilter: '',
+                sortByFilter: '',
+                teamsFilter: []
+            };
+        default: return state;
     }
 };
--- src/services/PlayersService.js
+++ src/services/PlayersService.js
@@ -4,6 +4,36 @@ class PlayersService {
     getAllWithTeam() {
         return Http.get('players');
     }
+
+    filterPlayers(players, name = '', teams = [], orderBy= '') {
+        let filteredPlayers = [...players];
+        if(name.length) {
+            filteredPlayers = filteredPlayers.filter(
+                player => {
+                    if(
+                        (`${player.first_name} ${player.last_name}`)
+                            .toLowerCase()
+                            .includes(name.toLowerCase()) ||
+                        (`${player.last_name} ${player.first_name}`)
+                            .toLowerCase()
+                            .includes(name.toLowerCase())
+                    ) {
+                        return true;
+                    }
+                    return false;
+                }
+            );
+        }
+        if(teams.length) {
+            const selectedTeamIds = teams.map(team => team.key);
+            filteredPlayers = filteredPlayers.filter(
+                player => {
+                    return selectedTeamIds.includes(player.team[0].id);
+                }
+            )
+        }
+        return filteredPlayers;
+    }
 }
 
 export default new PlayersService();
